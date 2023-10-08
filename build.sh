8d4d527f4439e55be1a9fd03bff7e5d7864c6cd4
Jennifer Chiou <jennifer.chiou@mail.utoronto.ca>
2023-10-07 22:42:46 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ pip install scipy
 pip install SpeechRecognition
 pip install pydub
 
-# for ml
-pip install questgen==0.4.1
-
 # install homebrew
 # actually u don't need to do this anymore lol
 #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
