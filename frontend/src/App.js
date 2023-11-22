af1e5a3453555cde03f68a843f2813a515843698
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2023-11-22 13:31:14 -0500

    copied react structure from geeks4geeks article; unused rn

---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+// SOURCE: https://www.geeksforgeeks.org/how-to-connect-reactjs-with-flask-api/
+
+// Importing modules
+import React, { useState, useEffect } from "react";
+import "./App.css";
+ 
+function App() {
+    // usestate for setting a javascript
+    // object for storing and using data
+    const [data, setdata] = useState({
+        name: "",
+        age: 0,
+        date: "",
+        programming: "",
+    });
+ 
+    // Using useEffect for single rendering
+    useEffect(() => {
+        // Using fetch to fetch the api from 
+        // flask server it will be redirected to proxy
+        fetch("/data").then((res) =>
+            res.json().then((data) => {
+                // Setting a data from api
+                setdata({
+                    name: data.Name,
+                    age: data.Age,
+                    date: data.Date,
+                    programming: data.programming,
+                });
+            })
+        );
+    }, []);
+ 
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>React and flask</h1>
+                {/* Calling a data from setdata for showing */}
+                <p>{data.name}</p>
+                <p>{data.age}</p>
+                <p>{data.date}</p>
+                <p>{data.programming}</p>
+ 
+            </header>
+        </div>
+    );
+}
+ 
+export default App;
