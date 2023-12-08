b9a7bf9249e176933dcacef7807d681f2d3b3332
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2023-12-08 18:07:13 -0500

    Update jennifer_openai.py

--- openai_practice/jennifer_openai.py
+++ openai_practice/jennifer_openai.py
@@ -5,32 +5,22 @@ from openai import OpenAI
 client = OpenAI(api_key='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')
 import gradio as gr
 
-def openai_chat(input_prompt):
-    # the template teaches openai api what u want to achieve
-    # i stored it in another file for more readability
-    with open('jennifer_template.txt', 'r') as file:
-        template = file.read()
+def openai_chat(elevator_pitch):
+    # https://platform.openai.com/docs/guides/text-generation/chat-completions-api LOL why didn't i read documentation
+    #elevator_pitch = """Hey, I’m Jamie Brown. I’ve just received my double bachelor’s degree in computer science and business administration. I’ve held three internships where I’ve had to create reports, develop new programs, and pitch projects to the wider team. My current focus is getting a job in project management."""
+    input_prompt = f"""{elevator_pitch}
+        Based on the above interview response, generate five insightful interview questions. For example:
+        1. "Can you describe a project from your internships where you faced a significant challenge and how you addressed it?"
+        2. "How do you envision applying your computer science skills in a project management role?"
+        3. "What strategies have you found effective for communicating technical concepts to non-technical team members during your pitches?"
+        These questions aim to probe deeper into the candidate's practical experience, problem-solving skills, and ability to bridge technical and business domains."""
 
-    # when user enters a new sentence, they obviously won't type "Sentence: " first, so we do that
-    input_prompt = 'Sentence: ' + input_prompt
-    # pass the input prompt AND the template into the completions create function
-    prompt = template + input_prompt
-    # more tokens means longer responses, higher temperature means more creative responses
-    completion = client.completions.create(model='davinci', prompt=str(prompt), max_tokens=64, temperature=0.7)
+    completion = client.completions.create(model='davinci', prompt=input_prompt, max_tokens=150, temperature=0.7)
     
     # extract the text section of completion object
     message = completion.choices[0].text
     print(f'{message=}')
-    
-    output_list = message.split('\n')
-    print(f'{output_list=}')
-    out_index = []
-    for idx, sentence in enumerate(output_list):
-        if 'Question' in sentence:
-            out_index.append(idx)
-    print(f'{out_index=}')
-    if out_index:
-        return output_list[min(out_index):max(out_index) + 1]
+    return message
 
 gr.Interface(fn=openai_chat, inputs=['text'], outputs=['text']).launch(debug=True)
 
